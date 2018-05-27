d74457def0c95b278adff11ed64fdf9d1770b859
Felix Roske <f.roske@roskenet.de>
2018-05-27 17:22:47 +0200

    Try to run Eureka service discovery

--- helloworld/src/main/java/de/roskenet/helloworld/HelloworldApplication.java
+++ helloworld/src/main/java/de/roskenet/helloworld/HelloworldApplication.java
@@ -2,8 +2,10 @@ package de.roskenet.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class HelloworldApplication {
 
 	public static void main(String[] args) {
