532532b7a32cc648772c11f44e72f4c9d058168f
Felix Roske <f.roske@roskenet.de>
2018-05-29 23:27:29 +0200

    Remove unused manifests

--- discovery/build-and-deploy.sh
+++
@@ -1,6 +0,0 @@
-export PATH=/home/felix/Packages/java-8/bin:$PATH
-./mvnw clean install
-eval $(minikube docker-env)
-docker build -t roskenet/discovery:1.3.0 .
-kubectl delete deployment discovery-depl
-kubectl create -f deploy/deployment.yaml
