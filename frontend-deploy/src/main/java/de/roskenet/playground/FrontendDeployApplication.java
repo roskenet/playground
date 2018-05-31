137c8c6ef4a8930f5c7a0196ecfa78926fdd3515
Felix Roske <f.roske@roskenet.de>
2018-05-31 23:35:54 +0200

    Add frontend deployment

---
+++ frontend-deploy/src/main/java/de/roskenet/playground/FrontendDeployApplication.java
@@ -0,0 +1,12 @@
+package de.roskenet.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontendDeployApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontendDeployApplication.class, args);
+	}
+}
---
+++ frontend-deploy/src/test/java/de/roskenet/playground/FrontendDeployApplicationTests.java
@@ -0,0 +1,16 @@
+package de.roskenet.playground;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FrontendDeployApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
