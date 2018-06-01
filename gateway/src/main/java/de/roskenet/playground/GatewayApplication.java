a6cbb57804746a134880b577add9fc183dda62cd
Felix Roske <f.roske@roskenet.de>
2018-06-01 19:00:38 +0200

    Ignore my temp helper projects

--- gateway/src/main/java/de/roskenet/playground/GatewayApplication.java
+++ gateway/src/main/java/de/roskenet/playground/GatewayApplication.java
@@ -2,9 +2,11 @@ package de.roskenet.playground;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableZuulProxy
 public class GatewayApplication {
 
