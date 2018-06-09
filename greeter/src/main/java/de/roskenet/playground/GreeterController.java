b943a8b61336341ab999e335f83804a10c19d30b
Felix Roske <f.roske@roskenet.de>
2018-06-09 11:46:39 +0200

    Add greeter app

---
+++ greeter/src/main/java/de/roskenet/playground/GreeterController.java
@@ -0,0 +1,26 @@
+package de.roskenet.playground;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreeterController {
+
+    public static class Greeting {
+        private String name;
+        
+        public Greeting(String name) {
+           this.name = name; 
+        }
+        
+        public String getName() {
+            return name;
+        }
+    }
+    
+    @GetMapping("/greet/{name}")
+    public Greeting greet(@PathVariable("name") String name) {
+       return new Greeting(name); 
+    }
+}
