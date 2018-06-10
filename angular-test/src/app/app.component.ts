259f0eb0ae068173fac33fd885cd549fb4ae8a6a
Felix Roske <f.roske@roskenet.de>
2018-06-10 12:09:10 +0200

    Add first frontend -> webservice call

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  name: string;
+
+  constructor(private http: Http) {
+    http.get('http://192.168.99.100:30100/greet/Felix')
+        .map(res => res.json())
+        .subscribe(val => this.name = val['name']);
+        // .subscribe(val => console.log(val['name']));
+  }
 }
--- greeter/src/main/java/de/roskenet/playground/GreeterController.java
+++ greeter/src/main/java/de/roskenet/playground/GreeterController.java
@@ -1,9 +1,11 @@
 package de.roskenet.playground;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin
 @RestController
 public class GreeterController {
 
