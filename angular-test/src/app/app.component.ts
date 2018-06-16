21a74e0868dd567e2061d5a7dfb37edae38cf0fd
Felix Roske <f.roske@roskenet.de>
2018-06-16 19:18:49 +0200

    Add latest code

--- angular-test/src/app/app.component.ts
+++ angular-test/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Http } from '@angular/http';
+//import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Component({
@@ -11,10 +11,12 @@ export class AppComponent {
   title = 'app';
   name: string;
 
-  constructor(private http: Http) {
-    http.get('http://192.168.99.100:30100/greet/Felix')
-        .map(res => res.json())
-        .subscribe(val => this.name = val['name']);
+//  constructor(private http: Http) {
+  constructor() {
+//    http.get('http://192.168.99.100:30100/greet/Felix')
+//        .map(res => res.json())
+//        .subscribe(val => this.name = val['name']);
         // .subscribe(val => console.log(val['name']));
+
   }
 }
--- angular-test/src/app/app.module.ts
+++ angular-test/src/app/app.module.ts
@@ -2,9 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
-
 import { AppComponent } from './app.component';
-
 
 @NgModule({
   declarations: [
