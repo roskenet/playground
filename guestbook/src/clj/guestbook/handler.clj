04bb61a5882ca46b85a9cbd4ad0ce3c2bf5e279b
Felix Roske <f.roske@roskenet.de>
2018-07-11 23:39:53 +0200

    Add a clojure example

---
+++ guestbook/src/clj/guestbook/handler.clj
@@ -0,0 +1,27 @@
+(ns guestbook.handler
+  (:require 
+            [guestbook.layout :refer [error-page]]
+            [guestbook.routes.home :refer [home-routes]]
+            [compojure.core :refer [routes wrap-routes]]
+            [ring.util.http-response :as response]
+            [guestbook.middleware :as middleware]
+            [compojure.route :as route]
+            [guestbook.env :refer [defaults]]
+            [mount.core :as mount]))
+
+(mount/defstate init-app
+  :start ((or (:init defaults) identity))
+  :stop  ((or (:stop defaults) identity)))
+
+(mount/defstate app
+  :start
+  (middleware/wrap-base
+    (routes
+      (-> #'home-routes
+          (wrap-routes middleware/wrap-csrf)
+          (wrap-routes middleware/wrap-formats))
+          (route/not-found
+             (:body
+               (error-page {:status 404
+                            :title "page not found"}))))))
+
