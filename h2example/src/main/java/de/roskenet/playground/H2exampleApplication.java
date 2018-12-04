c77d42dd65dd422748afee0ae5fc5cf516c6bc09
Felix Roske <felix@zalando.de>
2018-12-04 13:59:21 +0100

    Add h2 starter project

---
+++ h2example/src/main/java/de/roskenet/playground/H2exampleApplication.java
@@ -0,0 +1,12 @@
+package de.roskenet.playground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2exampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2exampleApplication.class, args);
+	}
+}
