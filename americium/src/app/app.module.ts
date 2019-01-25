e1d41694b15b868b0af2567258ef7947cf7a4496
Felix Roske <felix@zalando.de>
2019-01-25 12:13:37 +0100

    Add material design for angular

--- americium/src/app/app.module.ts
+++ americium/src/app/app.module.ts
@@ -1,4 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -10,7 +12,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatCheckboxModule,
     AppRoutingModule
+  ],
+  exports: [
+      MatButtonModule,
+      MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
