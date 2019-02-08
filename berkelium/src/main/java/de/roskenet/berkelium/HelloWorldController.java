06795134087dd0fe85ee8f4bd9d6790622581d4c
Felix Roske <felix@zalando.de>
2019-02-08 11:55:05 +0100

    Add new SecurityConfig

--- berkelium/src/main/java/de/roskenet/berkelium/HelloWorldController.java
+++ berkelium/src/main/java/de/roskenet/berkelium/HelloWorldController.java
@@ -1,5 +1,6 @@
 package de.roskenet.berkelium;
 
+import javax.annotation.security.PermitAll;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping
-    @PreAuthorize("permitAll()")
+//    @PreAuthorize("permitAll()")
+//    @PreAuthorize("hasRole('USER')")
     public String helloWorld() {
         return "{\"answer\": \"Hello World\"}";
     }
--- berkelium/src/main/java/de/roskenet/berkelium/SecurityConfig.java
+++ berkelium/src/main/java/de/roskenet/berkelium/SecurityConfig.java
@@ -1,10 +1,28 @@
 package de.roskenet.berkelium;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class SecurityConfig {
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+    }
+
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("password").roles("USER")
+//            .and()
+//                .withUser("admin").password("password").roles("ADMIN");
+//    }
 }
