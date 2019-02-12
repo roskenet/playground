3a9fbc7fd8c867ee0b31b313721f2d201c13eab3
Felix Roske <felix@zalando.de>
2019-02-12 10:45:46 +0100

    Add correct config

--- arsenium/src/main/java/de/roskenet/arsenium/ArseniumApplication.java
+++ arsenium/src/main/java/de/roskenet/arsenium/ArseniumApplication.java
@@ -2,8 +2,10 @@ package de.roskenet.arsenium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
 public class ArseniumApplication {
 
 	public static void main(String[] args) {
