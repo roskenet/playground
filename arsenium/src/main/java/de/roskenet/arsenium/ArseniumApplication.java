835021fe0032f5e58bceb7049cde892f3c072e94
Felix Roske <felix@zalando.de>
2019-02-12 10:27:31 +0100

    Add arsenium project

---
+++ arsenium/src/main/java/de/roskenet/arsenium/ArseniumApplication.java
@@ -0,0 +1,14 @@
+package de.roskenet.arsenium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArseniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArseniumApplication.class, args);
+	}
+
+}
+
