1afb1c63eb479373dbd794bbdaad97940a39a7a3
Felix Roske <felix@zalando.de>
2019-02-12 13:22:47 +0100

    Add arsenium spring gateway

--- arsenium/src/main/java/de/roskenet/arsenium/ArseniumApplication.java
+++ arsenium/src/main/java/de/roskenet/arsenium/ArseniumApplication.java
@@ -2,10 +2,8 @@ package de.roskenet.arsenium;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
-@EnableZuulProxy
 public class ArseniumApplication {
 
 	public static void main(String[] args) {
