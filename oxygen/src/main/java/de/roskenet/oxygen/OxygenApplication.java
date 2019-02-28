5739dca1086295e7d2eb81655001f7d282fae3cc
Felix Roske <felix@zalando.de>
2019-02-28 15:35:56 +0100

    Make oxygen a real WebApp

--- oxygen/src/main/java/de/roskenet/oxygen/OxygenApplication.java
+++ oxygen/src/main/java/de/roskenet/oxygen/OxygenApplication.java
@@ -1,19 +1,13 @@
 package de.roskenet.oxygen;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class OxygenApplication implements CommandLineRunner {
+public class OxygenApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OxygenApplication.class, args);
 	}
-
-    @Override
-    public void run(String... args) throws Exception {
-       System.out.println("Hello World!"); 
-    }
 
 }
