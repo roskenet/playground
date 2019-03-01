b8a750075f5a02001f4fcd633a40475982cf67ac
Felix Roske <felix@zalando.de>
2019-03-01 16:08:25 +0100

    Add first export test

--- keycloak/scripts.sh
+++ keycloak/scripts.sh
@@ -1,6 +1,5 @@
 function ktoken() {
 
-TOKEN=$(curl --silent -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://192.168.99.100:30111/auth/realms/master/protocol/openid-connect/token" | jq .access_token | sed -e 's/^"//' -e 's/"$//')
+echo $(curl --silent -d "client_id=admin-cli" -d "username=admin" -d "password=admin" -d "grant_type=password" "http://192.168.99.100:30111/auth/realms/master/protocol/openid-connect/token" | jq .access_token | sed -e 's/^"//' -e 's/"$//')
 
-echo $TOKEN
 }
