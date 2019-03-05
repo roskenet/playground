f776450691f92bc8c92d216ba4f6011c0b243c1c
Felix Roske <felix@zalando.de>
2019-03-05 13:32:31 +0100

    Add vanadium example

---
+++ vanadium-backend/src/main/java/de/roskenet/vanadium/VanadiumBackendApplication.java
@@ -0,0 +1,14 @@
+package de.roskenet.vanadium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanadiumBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanadiumBackendApplication.class, args);
+	}
+
+}
+
