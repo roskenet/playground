522641aaa405e3cdfc18cdfecb9262cf1f640b94
Felix Roske <felix@zalando.de>
2019-10-04 11:21:47 +0200

    Keep everything in sync

--- americium/src/app/app.module.ts
+++ americium/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatButtonModule, MatListModule, MatCheckboxModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -15,6 +15,7 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatListModule,
     AppRoutingModule
   ],
   exports: [
