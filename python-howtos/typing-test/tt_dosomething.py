fae4cfa5d726f46d305bff2a55eba8c65c961ca5
Felix Roske <felix@zalando.de>
2019-11-15 13:14:42 +0100

    Add more docu

--- python-howtos/typing-test/tt_dosomething.py
+++ python-howtos/typing-test/tt_dosomething.py
@@ -5,6 +5,15 @@ Created on Nov 13, 2019
 '''
 import typing
 
+mystring = '''{{"production": 
+                {{ "totalCount": {} }} 
+              ,
+              "test":
+                {{ "totalCount" : {} }} 
+              }}'''.format(21, 42)
+
+print(mystring)
+
 def do_something(name: str) -> str: 
    print("Hello World") 
    return "Hello"
