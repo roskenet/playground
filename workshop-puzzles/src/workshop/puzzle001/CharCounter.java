b468a1616b4dc5c989c81eba8a829d63ee945346
Felix Roske <felix@zalando.de>
2019-12-02 13:23:35 +0100

    Add CharCounter puzzle

--- workshop-puzzles/src/workshop/puzzle001/CharCounter.java
+++ workshop-puzzles/src/workshop/puzzle001/CharCounter.java
@@ -1,11 +1,23 @@
 package workshop.puzzle001;
 
+import java.util.HashMap;
+import java.util.Map;
+
 // TODO:
-// Write a program that counts duplicate characters from a given string.
-class DuplicateCharCounter {
+// Write a program that counts characters in a given string.
+class CharCounter {
 
-    public static void main(String[] args) {
-        
-    }
+    public static Map<Character, Integer> countCharacters(String str) {
 
+        Map<Character, Integer> result = new HashMap<>();
+
+        // or use for(char ch: str.toCharArray()) { ... }
+        for (int i = 0; i<str.length(); i++) {
+          char ch = str.charAt(i); 
+
+          result.compute(ch, (k, v) -> (v == null) ? 1 : ++v);
+        }
+
+        return result;
+      }
 }
--- workshop-puzzles/src/workshop/puzzle001/Tests.java
+++ workshop-puzzles/src/workshop/puzzle001/Tests.java
@@ -1,14 +1,30 @@
 package workshop.puzzle001;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 
 class Tests {
 
     @Test
-    void test() {
-        fail("Not yet implemented");
+    void testCharCounter_i() {
+        var input = "Mississippi";
+        
+        Map<Character, Integer> result = CharCounter.countCharacters(input);
+        
+        assertThat(result.get('i'), is(4));
     }
 
+    @Test
+    void testCharCounter_M() throws Exception {
+        var input = "Magdeburg";
+        
+        Map<Character, Integer> result= CharCounter.countCharacters(input);
+        
+        assertThat(result.get('M'), is(1));
+    }
 }
