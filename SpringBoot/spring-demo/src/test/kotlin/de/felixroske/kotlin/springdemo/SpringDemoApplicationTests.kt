29735ee317038e4bf3fe1433777b99c809450eb5
Felix Roske <felix@zalando.de>
2020-01-06 16:43:08 +0100

    Add new SpringBoot

---
+++ SpringBoot/spring-demo/src/test/kotlin/de/felixroske/kotlin/springdemo/SpringDemoApplicationTests.kt
@@ -0,0 +1,13 @@
+package de.felixroske.kotlin.springdemo
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDemoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
---
+++ sandbox/src/de/felixroske/sandbox/MyClass.java
@@ -0,0 +1,10 @@
+package de.felixroske.sandbox;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+        
+        int currentInt = null;
+        
+    }
+}
