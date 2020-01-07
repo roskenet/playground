57df0b578867360098ba9c39e40a2d73d8c73d15
Felix Roske <felix@zalando.de>
2020-01-07 12:07:12 +0100

    Add basic projects under Workshop

--- sandbox/src/de/felixroske/sandbox/MyClass.java
+++ sandbox/src/de/felixroske/sandbox/MyClass.java
@@ -3,8 +3,22 @@ package de.felixroske.sandbox;
 public class MyClass {
 
     public static void main(String[] args) {
+
+        int currentInt;
+
+        MyClass myClass = new MyClass();
         
-        int currentInt = null;
         
+        myClass.doSomething(Integer.valueOf("42"));
+    }
+
+    public Integer doSomething(int x) {
+
+        switch (x) {
+        case 1:
+            
+            
+        }
+        return Integer.valueOf(x);
     }
 }
