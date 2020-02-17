ded2e80217df5731066e7cbdbb70d1be5c41930c
Felix Roske <felix@zalando.de>
2020-02-17 17:51:20 +0100

    Add ant design to it

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Layout } from 'antd';
+import 'antd/dist/antd.css';
+
+const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World! This is another amazing test text.</p>
-        <p>Cool stuff!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header style={{ padding: 0, background: '#fff'}}>
+        <h1>Hello World!</h1>
+        </Header>
+        <Content style={{ padding: '0 50px', marginTop: 64 }}>
+        <h1>Hello World!</h1>
+        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+          sed diam nonumy eirmod tempor invidunt ut labore et dolore
+          magna aliquyam erat, sed diam voluptua. At vero eos et accusam
+          et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+          no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+          dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+          eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>
+        <p>At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
+          gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+          amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr,
+          sed diam nonumy eirmod tempor invidunt ut labore et
+          dolore magna aliquyam.</p>
+          </Content>
+          <Footer>
+            <p>This is a footer!</p>            
+          </Footer>
+       </Layout>
     </div>
   );
 }
