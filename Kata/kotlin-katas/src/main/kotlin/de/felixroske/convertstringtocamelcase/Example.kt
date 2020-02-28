4ce31bbc11bd4bb57bba67ed4b2bccbe112c1770
Felix Roske <felix@zalando.de>
2020-02-28 19:01:46 +0100

    Refactor Kata

--- Kata/kotlin-katas/src/main/kotlin/de/felixroske/convertstringtocamelcase/Example.kt
+++ Kata/kotlin-katas/src/main/kotlin/de/felixroske/convertstringtocamelcase/Example.kt
@@ -1,22 +1,10 @@
 package de.felixroske.convertstringtocamelcase
 
-fun main() {
-
-    val myString = "the_stealth_warrior"
-    val regex = """[-_]\w""".toRegex()
-
-    val s = regex.replace(myString) {
-        it.value.get(1).toString().toUpperCase()
-    }
-
-    println(s)
-}
-
-fun toCamelCase(str: String):String {
+fun toCamelCase(str: String): String {
     val regex = """[-_]\w""".toRegex()
 
     val s = regex.replace(str) {
-        it.value.get(1).toString().toUpperCase()
+        it.value[1].toString().toUpperCase()
     }
-   return s
+    return s
 }
