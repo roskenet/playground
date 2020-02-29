3262224aafcb34371ad7b4cda038f4d2f4e31e08
Felix Roske <f.roske@roskenet.de>
2020-02-29 18:12:17 +0100

    Refactor Kotlin

--- Kata/kotlin-katas/src/main/kotlin/de/felixroske/sumofoddnumbers/Kata.kt
+++ Kata/kotlin-katas/src/main/kotlin/de/felixroske/sumofoddnumbers/Kata.kt
@@ -4,9 +4,8 @@ fun rowSumOddNumbers(n: Int): Int {
     val startBy = (n - 1) * n + 1
     var sum = 0
 
-    for (x in 0..n-1) {
-       sum += (startBy + x*2)
-    }
+    for (x in 0..n-1) sum += (startBy + x*2)
+
 
     return sum
 }
