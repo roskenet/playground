cda69beda3bbb84f988841616338dac4e4c61454
Felix Roske <f.roske@roskenet.de>
2020-02-29 18:49:03 +0100

    Add more c/cpp code

--- Kata/cpp-kata/bitcounting.cpp
+++ Kata/cpp-kata/bitcounting.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+unsigned int countBits(unsigned long long n){
+	std::cout << "Hier bin ich: " << 8*sizeof(int) << " Bits für int";
+	return 0;
+}
+
+unsigned int doSomething(char x) {
+	auto y = 42ul;
+
+	return 0;
+}
+
 int main(int argc, char **argv) {
 	std::cout << "Hello World!";
+	countBits(42);
 	return 0;
 }
