578e93d1701981fc0e26135be0765ffcb7ca5102
Felix Roske <felix@zalando.de>
2020-03-02 14:36:18 +0100

    Add caching for easydiagonal

--- Kata/python-kata/theofficesabbatical/easydiagonal.py
+++ Kata/python-kata/theofficesabbatical/easydiagonal.py
@@ -3,15 +3,28 @@ Created on Mar 1, 2020
 
 @author: felix
 '''
+import fractions
+
+cache = {}
 
 def coeff(n, k):
-    if(2 * k > n) :
+
+    if(2 * k > n):
         k = n - k
     
-    result = 1
+    if cache.get(n) != None:
+        if cache.get(n).get(k) != None:
+            return cache.get(n).get(k)
+
+    result = fractions.Fraction(1)
     for i in range(1, k+1):
-        result = result * ((n - k + i) / i)
+        temp = fractions.Fraction((n - k + fractions.Fraction(i)) / fractions.Fraction(i)) 
+        result = result * temp
     
+    if cache.get(n) == None:
+        cache[n] = {}
+
+    cache[n][k] = result
     return result
 
 def diagonal(n, p):
@@ -21,4 +34,7 @@ def diagonal(n, p):
 
     return result
 
+
 print(diagonal(20, 5)) #Should be 54264
+
+#print(coeff(14,5))
