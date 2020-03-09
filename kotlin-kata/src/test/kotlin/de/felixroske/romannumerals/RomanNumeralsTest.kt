ba6c6147ef31bbf293ba76a352ea2130ad8ba657
Felix Roske <f.roske@roskenet.de>
2020-03-09 20:04:01 +0100

    Add roman numbers kata

---
+++ kotlin-kata/src/test/kotlin/de/felixroske/romannumerals/RomanNumeralsTest.kt
@@ -0,0 +1,23 @@
+package de.felixroske.romannumerals
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+/*
+# Conversion to roman numbers
+
+The infamous 'RomanNumberKata'
+
+Your task is to write a function that returns the roman number representing
+an arbitrary integer in the range of 1 - 3000.
+*/
+
+class TestKata {
+    @Test
+    fun testSimpleThings() {
+        assertEquals("IV", toRomanString(4))
+        assertEquals("CLVIII", toRomanString(158))
+        assertEquals("MMXX", toRomanString(2020))
+    }
+
+}
