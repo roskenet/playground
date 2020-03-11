42f2073441bb0209b0d54044d94c62a79984e585
Felix Roske <felix@zalando.de>
2020-03-11 15:21:54 +0100

    Comment out test code

--- kotlin-kata/src/test/kotlin/de/felixroske/tortoiseracing/TortoiseRacingTest.kt
+++ kotlin-kata/src/test/kotlin/de/felixroske/tortoiseracing/TortoiseRacingTest.kt
@@ -42,14 +42,16 @@ or Google: "convert decimal time to hours minutes seconds"
 */
 
 import java.util.Arrays
-import org.junit.Assert.*
+import org.junit.Assert.assertArrayEquals
 import org.junit.Test
 
 class TortoiseTest {
+/*
     @Test
     fun test1() {
         println("Basic Tests")
         assertArrayEquals(intArrayOf(0, 32, 18), race(720, 850, 70))
         assertArrayEquals(intArrayOf(3, 21, 49), race(80, 91, 37))
     }
+*/
 }
