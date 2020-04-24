32a058edc86ea2c92bce3439e1d4c762873fa6a7
Felix Roske <felix@roskenet.de>
2020-04-25 00:08:11 +0200

    Add running mockmvc example

--- books/SpringBoot2/mockmvc-starter/src/test/kotlin/de/felixroske/examples/mockmvcstarter/MockmvcStarterApplicationTests.kt
+++ books/SpringBoot2/mockmvc-starter/src/test/kotlin/de/felixroske/examples/mockmvcstarter/MockmvcStarterApplicationTests.kt
@@ -1,13 +1,27 @@
 package de.felixroske.examples.mockmvcstarter
 
 import org.junit.jupiter.api.Test
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.MediaType
+import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.get
 
-@SpringBootTest
-class MockmvcStarterApplicationTests {
+//@SpringBootTest
+@WebMvcTest
+class MockmvcStarterApplicationTests() {
+
+	@Autowired
+	lateinit private var mockMvc: MockMvc
 
 	@Test
-	fun contextLoads() {
+	fun `Test stocks ticker endpoint`() {
+		mockMvc.get("/api/stock/ME") {
+			accept = MediaType.APPLICATION_JSON
+		}.andExpect {
+			status { isOk }
+			content { string("Wirecard SE") }
+		}
 	}
 
 }
