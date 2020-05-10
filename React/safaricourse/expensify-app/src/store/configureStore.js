5894c3bbdcb1df251eae45ceab0146dc11cccfab
Felix Roske <felix@roskenet.de>
2020-05-10 16:59:47 +0200

    Add express server

--- React/safaricourse/expensify-app/src/store/configureStore.js
+++ React/safaricourse/expensify-app/src/store/configureStore.js
@@ -10,6 +10,5 @@ export default () => {
         }),
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     );
-
     return store;
 };
--- React/safaricourse/expensify-app/webpack.config.js
+++ React/safaricourse/expensify-app/webpack.config.js
@@ -1,30 +1,41 @@
 const path = require('path');
+const ExtraxtTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output: {
-        path: path.join(__dirname, 'public'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
+module.exports = (env) => {
+    console.log('env', env);
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtraxtTextPlugin('styles.css');
+
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                })
+            }
             ]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
         }
-    ]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
     }
 };
 
