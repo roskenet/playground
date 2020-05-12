cf38f5fc8fbf1ff12d545a693fec054997cbac32
Felix Roske <felix@roskenet.de>
2020-05-12 02:16:19 +0200

    Add a react howto

---
+++ React/howtos/ht001-antd/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Button } from 'antd';
+
+const App = () => {
+  return (
+    <div>
+      <Button type="primary">Hello, Ant Design!</Button>
+      <a href="foo.bar">I'm a link. Click me, please!</a>
+      <p>Dies ist mein Absatz.</p>
+    </div>
+  );
+};
+
+export default App;
+
