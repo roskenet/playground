68304378c3c6acc3a235c0b17603d6915d1fc764
Felix Roske <felix@roskenet.de>
2020-12-13 18:30:02 +0100

    Add C++ HelloWorld

---
+++ Cpp/hello_cpp/cmake-build-debug/CMakeFiles/hello_cpp.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hello_cpp.dir/main.cpp.o"
+  "hello_cpp"
+  "hello_cpp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello_cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
