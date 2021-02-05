c9b934727d0a96b6e551643d50931081f3fed3bc
Felix Roske <felix@roskenet.de>
2021-02-05 19:03:28 +0100

    Add SpringExamples

--- python/hello-python/main.py
+++ python/hello-python/main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+#
 # This is a sample Python script.
 
 # Press Shift+F10 to execute it or replace it with your code.
@@ -7,7 +9,6 @@
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+8 to toggle the breakpoint.
-
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
