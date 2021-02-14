5a3ef666ddf27a69f45e60c357e386d7d42860dd
Felix Roske <felix@roskenet.de>
2021-02-14 23:23:06 +0100

    Add openapi comment

--- ChristmasTree/web-service/src/main/java/de/roskenet/christmastree/webservice/TreeService.java
+++ ChristmasTree/web-service/src/main/java/de/roskenet/christmastree/webservice/TreeService.java
@@ -2,6 +2,7 @@ package de.roskenet.christmastree.webservice;
 
 
 import de.roskenet.christmastree.ChristmasTreeNursery;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ public class TreeService {
     @Autowired
     private ChristmasTreeNursery nursery;
 
+    @Operation(summary = "Get a christmas tree with the given height")
     @GetMapping("/tree")
     public TreeResponse tree(@RequestParam("height") int height) {
         return new TreeResponse(nursery.tree(height));
