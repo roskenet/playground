c4848e2166414e2209ce72219ae0cec89db443f7
Felix Roske <felix@roskenet.de>
2021-03-03 23:43:46 +0100

    Add a new React Example

--- SpringBoot/spring-cli/src/main/java/de/roskenet/springcliplayground/SpringCliPlaygroundApplication.java
+++ SpringBoot/spring-cli/src/main/java/de/roskenet/springcliplayground/SpringCliPlaygroundApplication.java
@@ -1,5 +1,6 @@
 package de.roskenet.springcliplayground;
 
+import javax.inject.Inject;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,13 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringCliPlaygroundApplication implements CommandLineRunner {
 
+    @Inject
+    private FlxWorker theWorker;
+
     public static void main(String[] args) {
         SpringApplication.run(SpringCliPlaygroundApplication.class, args);
     }
 
     @Override
     public void run(String... args) {
-        System.out.println("Hello World!");
+        System.out.println(theWorker.doSomething("Hello World!"));
     }
 
 }
