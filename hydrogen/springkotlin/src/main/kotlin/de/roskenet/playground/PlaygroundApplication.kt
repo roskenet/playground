5562ec4184710184e8ee085780c4f099e1737d24
Felix Roske <felix@roskenet.de>
2021-03-07 01:39:07 +0100

    Add kotling spring helloworld

---
+++ hydrogen/springkotlin/src/main/kotlin/de/roskenet/playground/PlaygroundApplication.kt
@@ -0,0 +1,11 @@
+package de.roskenet.playground
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PlaygroundApplication
+
+fun main(args: Array<String>) {
+    runApplication<PlaygroundApplication>(*args)
+}
