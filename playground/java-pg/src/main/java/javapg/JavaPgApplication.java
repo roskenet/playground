53623f89f67dde525584fbb72bb78625a9a5d10e
Felix Roske <felix@roskenet.de>
2021-04-16 23:01:33 +0200

    Add more javapg

--- playground/java-pg/src/main/java/javapg/JavaPgApplication.java
+++ playground/java-pg/src/main/java/javapg/JavaPgApplication.java
@@ -1,13 +1,18 @@
 package javapg;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JavaPgApplication {
+public class JavaPgApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaPgApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("Hello world");
+    }
 }
