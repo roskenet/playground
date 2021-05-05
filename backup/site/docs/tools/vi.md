63e1121d2ec35d0c3afca4fe9ba8e5f67b01142b
Felix Roske <felix@roskenet.de>
2021-05-05 11:48:07 +0200

    Reset folder

--- backup/site/docs/tools/vi.md
+++
@@ -1,7 +0,0 @@
-# Vi and Vim
-
-`Ctrl`+`O` Springe zurueck.
-
-`Ctrl`+`I` Springe vor.
-
-`Ctrl` + `X` `Ctrl` + `E` Öffne einen editor
---
+++ backups/Java/spring-pg/src/test/java/de/roskenet/pg/springpg/SpringPgApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roskenet.pg.springpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
