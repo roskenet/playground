bdcf7358be44bdbd0c090622345029bb0c7a434a
Felix Roske <felix@roskenet.de>
2021-05-12 23:20:38 +0200

    Add extends example

--- snippet_001/src/de/roskenet/basicjava/JavaHelloWorld.java
+++ snippet_001/src/de/roskenet/basicjava/JavaHelloWorld.java
@@ -1,5 +1,9 @@
 package de.roskenet.basicjava;
 
+/*
+    Jede Einführung in eine neue Programmiersprache hat mit
+    "Hello World" zu beginnen.
+ */
 public class JavaHelloWorld {
 
     public static void main(String[] args) {
---
+++ snippet_001/src/de/roskenet/basicjava/Sub.java
@@ -0,0 +1,5 @@
+package de.roskenet.basicjava;
+
+public class Sub extends Base {
+
+}
