3fa5a1bde5c49c6c7189eca528cf0e6a36095c9c
Felix Roske <felix@roskenet.de>
2021-05-21 19:52:43 +0200

    Add graaljar example

--- java/part1/src/main/java/de/roskenet/learnjava/App.java
+++ java/part1/src/main/java/de/roskenet/learnjava/App.java
@@ -1,9 +1,13 @@
-package de.roskenet.graalvm;
+package de.roskenet.learnjava;
 
 public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+        byte aByte = 0;
+
+        System.out.println("aByte is: " + aByte);
     }
 }
