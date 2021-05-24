6a2180c52a27b361c204a39316adfcb3d9417c67
Felix Roske <felix@roskenet.de>
2021-05-24 12:23:26 +0200

    Add my github pages

--- java/graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
+++ java/graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
@@ -1,9 +1,18 @@
 package de.roskenet.graalvm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        System.out.println("What's your name, buddy? ");
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            String name = br.readLine();
+            System.out.println("Hello, " + name);
+        } catch (IOException e) {
+            System.out.println("Some unrecoverable error happened. I better quit!");
+        }
     }
 }
