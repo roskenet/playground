cfa47656bf85e89d7dd56599d00de60543fa6428
Felix Roske <felix@roskenet.de>
2021-05-31 23:18:13 +0200

    Play with classes

--- Java/Snippets/src/de/roskenet/Main.java
+++ Java/Snippets/src/de/roskenet/Main.java
@@ -2,10 +2,13 @@ package de.roskenet;
 
 public class Main {
 
+    public static class TheNestedClass {
+        public void doSomething() {
+            System.out.println("Hello World");
+        }
+    }
+
     public static void main(String[] args) {
         String[] myArray = {"eins", "zwei", "drei"};
-
-
-
     }
 }
