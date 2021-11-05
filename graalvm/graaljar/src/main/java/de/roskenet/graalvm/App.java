fe1af0154e2c8fbf58b3040a7c83f2753f9abd2f
Felix Roske <felix@zalando.de>
2021-11-05 11:20:48 +0100

    Fix merge conflicts

--- graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
+++ graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
@@ -6,20 +6,9 @@ import java.io.InputStreamReader;
 
 public class App
 {
-<<<<<<< HEAD:graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
         System.out.println("This is a message from a native compiled application.");
-=======
-    public static void main( String[] args ) {
-        System.out.println("What's your name, buddy? ");
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String name = br.readLine();
-            System.out.println("Hello, " + name);
-        } catch (IOException e) {
-            System.out.println("Some unrecoverable error happened. I better quit!");
-        }
->>>>>>> origin/master:java/graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
     }
 }
