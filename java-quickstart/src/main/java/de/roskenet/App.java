2db549d07eefcfd3be20529588b915adcb074fbe
Felix Roske <felix@roskenet.de>
2021-11-20 22:10:23 +0100

    Format code

--- java-quickstart/src/main/java/de/roskenet/App.java
+++ java-quickstart/src/main/java/de/roskenet/App.java
@@ -1,13 +1,8 @@
 package de.roskenet;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
     }
 }
