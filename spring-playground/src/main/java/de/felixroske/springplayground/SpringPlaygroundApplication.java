280587c61514d44c1d83dfe73808f36bdb08ee51
Felix Roske <felix@roskenet.de>
2021-11-21 10:46:53 +0100

    Add a SpringBoot project

---
+++ spring-playground/src/main/java/de/felixroske/springplayground/SpringPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package de.felixroske.springplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPlaygroundApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPlaygroundApplication.class, args);
+	}
+
+}
