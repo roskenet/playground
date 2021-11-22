60444a98a1617f0542e3d8d28d2fa40572e83908
Felix Roske <felix@roskenet.de>
2021-11-22 09:34:31 +0100

    Add a web service REST example

---
+++ spring-boot-wiremock/src/test/java/de/roskenet/bootwiremock/SpringBootWiremockApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roskenet.bootwiremock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWiremockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
