9286190e376bd025c83a418f85a317c5fd270a31
Felix Roske <felix@roskenet.de>
2021-12-03 15:19:19 +0100

    Sync all

--- graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
+++ graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
@@ -1,25 +1,9 @@
 package de.roskenet.graalvm;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class App
 {
-<<<<<<< HEAD:graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("This is a message from a native compiled application.");
-=======
-    public static void main( String[] args ) {
-        System.out.println("What's your name, buddy? ");
-        try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            String name = br.readLine();
-            System.out.println("Hello, " + name);
-        } catch (IOException e) {
-            System.out.println("Some unrecoverable error happened. I better quit!");
-        }
->>>>>>> origin/master:java/graalvm/graaljar/src/main/java/de/roskenet/graalvm/App.java
     }
 }
