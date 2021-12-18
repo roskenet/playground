269cb21d4b0398109145bd97d77ace21964a4898
Felix Roske <felix@zalando.de>
2021-12-18 12:09:31 +0100

    Add hello-java

---
+++ hello-java/src/test/java/de/felixroske/AppTest.java
@@ -0,0 +1,14 @@
+package de.felixroske;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat( true ).isTrue();
+    }
+}
