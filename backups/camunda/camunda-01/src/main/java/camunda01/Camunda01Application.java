8698f08ff92ed3ade041d85e342284f07f33f3cc
Felix Roske <felix@roskenet.de>
2021-12-29 12:58:55 +0100

    Add musicdb

--- backups/camunda/camunda-01/src/main/java/camunda01/Camunda01Application.java
+++ backups/camunda/camunda-01/src/main/java/camunda01/Camunda01Application.java
@@ -1,8 +1,4 @@
-<<<<<<< HEAD:camunda/camunda-01/src/main/java/camunda01/Camunda01Application.java
 package camunda01;
-=======
-package javapg.spring;
->>>>>>> pp/master:camunda/camunda-01/src/main/java/camunda01/spring/JavaPgApplication.java
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ musicdb/tables.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS filesystem (
+    id BIGSERIAL,
+    md5sum TEXT,
+    filename TEXT
+    );
