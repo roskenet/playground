8471118e494c1bc86fb5efda88d00613c21afe26
Felix Roske <felix@roskenet.de>
2021-12-30 23:48:10 +0100

    Show webservice call result

--- ReactSpringBoot/Chapter06/myapp/src/App.js
+++ ReactSpringBoot/Chapter06/myapp/src/App.js
@@ -2,23 +2,27 @@ import './App.css';
 import {Button} from "antd";
 import {useState} from "react";
 
-const doSomething = () => {
-    console.log("Ein Click!");
 
-    fetch('http://oxygen.192-168-49-2.nip.io/api/name/somebody')
-        .then(response => response.json())
-        .then(result => console.log(result))
-            .catch(error => console.error(error));
-
-}
 
 const App = () => {
     const [theState, setTheState] = useState({"name": "Unknown"});
 
+const doSomething = () => {
+    console.log("Ein Click!");
+
+    fetch('http://oxygen.192-168-49-2.nip.io/api/name/some%20body')
+        .then(response => response.json())
+        .then(result => setTheState({"name": result.value}))
+        .catch(error => console.error(error));
+        // .then(result => console.log(result))
+        //     .catch(error => console.error(error));
+
+}
+
     return (
         <div className="App">
             <header className="App-header">
-                <p>Dies ist ein Test!</p>
+                <p>Dies is a real Killer App!</p>
                 <Button onClick={doSomething}>Click me!</Button>
                 <p>The State is: {theState.name}</p>
             </header>
