db1f3150f36887f4a8f8972c0be2e7dcfaf83b98
Felix Roske <felix@roskenet.de>
2022-01-12 10:47:16 +0100

    Add aluminium project

---
+++ chemicals/aluminium/src/main/java/de/felixroske/AluminiumApp.java
@@ -0,0 +1,11 @@
+package de.felixroske;
+
+/**
+ * Hello world!
+ */
+public class AluminiumApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
