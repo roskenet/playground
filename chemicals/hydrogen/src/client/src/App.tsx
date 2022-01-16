56290604a5094fee47bdca4a28bf29e3656f86e0
Felix Roske <felix@roskenet.de>
2022-01-16 18:58:45 +0100

    Add typescript and yarn

--- chemicals/hydrogen/src/client/src/App.tsx
+++ chemicals/hydrogen/src/client/src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello World!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1> Hello World! </h1>
+        <p>Dies ist ein Test!</p>
+      </div>
   );
 }
 
