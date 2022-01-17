760fb0fa8bddac99d920be06fa2fbc4d01bb2f7b
Felix Roske <felix@zalando.de>
2022-01-17 13:23:38 +0100

    Add embedded postgres

--- chemicals/arsenic/src/main/java/de/roskenet/arsenic/ChemelementController.java
+++ chemicals/arsenic/src/main/java/de/roskenet/arsenic/ChemelementController.java
@@ -4,13 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController
 public class ChemelementController {
 
     @Autowired
     private ChemelementRepository repository;
 
-    @GetMapping("elements")
+    @GetMapping("/api/elements")
     public Iterable<Chemelement> getChemelements() {
         return repository.findAll();
     }
