4d7f931a0fc05d14cdd1c40af8c81bc0743a0d26
Felix Roske <felix@zalando.de>
2022-01-17 11:28:25 +0100

    Add a JPA example

---
+++ chemicals/arsenic/src/test/java/de/roskenet/arsenic/ArsenicApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roskenet.arsenic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArsenicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
