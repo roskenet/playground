0d375ccd4e6acba22922c9e71c8529e41acf01e9
Felix Roske <felix@roskenet.de>
2022-01-17 20:42:36 +0100

    First working form example

--- chemicals/hydrogen/src/client/src/Form/FormExample.tsx
+++ chemicals/hydrogen/src/client/src/Form/FormExample.tsx
@@ -2,15 +2,20 @@ import {Button, Checkbox, Form, Input} from "antd";
 import {useState} from "react";
 
 const FormExample = () => {
-  const [theState, setTheState] = useState({"name": "Unknown"});
+  const [something, setSomething] = useState({"name": "Unknown", "number": 42});
 
-  const doSomething = () => {
+  const onFinish = () => {
+    // e.preventDefault()
     console.log("Ein Click!");
 
-    fetch('http://oxygen.192-168-49-2.nip.io/api/name/some%20body')
+    fetch('http://localhost:8080/api/something', {
+      method: 'POST',
+      body: JSON.stringify(something),
+      headers: {'Content-Type': 'application/json'},
+    })
     .then(response => response.json())
-    .then(result => setTheState({"name": result.value}))
-    .catch(error => console.error(error));
+    // .then(result => setTheState({"name": result.value}))
+    // .catch(error => console.error(error));
     // .then(result => console.log(result))
     //     .catch(error => console.error(error));
   }
@@ -22,24 +27,16 @@ const FormExample = () => {
         labelCol={{ span: 8 }}
         wrapperCol={{ span: 16 }}
         initialValues={{ remember: true }}
-        // onFinish={onFinish}
+        onFinish={onFinish}
         // onFinishFailed={onFinishFailed}
         autoComplete="off"
         >
         <Form.Item
             label="Username"
-            name="username"
+            name="something[name]"
             rules={[{ required: true, message: 'Please input your username!' }]}
         >
-          <Input />
-        </Form.Item>
-
-        <Form.Item
-            label="Password"
-            name="password"
-            rules={[{ required: true, message: 'Please input your password!' }]}
-        >
-          <Input.Password />
+          <Input onChange={e => setSomething({...something, name: e.target.value})}/>
         </Form.Item>
 
         <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 8, span: 16 }}>
