c816266dc0aa3f53cc1aca23e9a4e88f97116d12
Felix Roske <felix@zalando.de>
2022-01-17 12:36:24 +0100

    Add arsenic jpa example

---
+++ chemicals/arsenic/src/main/java/de/roskenet/arsenic/ChemelementController.java
@@ -0,0 +1,17 @@
+package de.roskenet.arsenic;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+public class ChemelementController {
+
+    @Autowired
+    private ChemelementRepository repository;
+
+    @GetMapping("elements")
+    public Iterable<Chemelement> getChemelements() {
+        return repository.findAll();
+    }
+}
---
+++ chemicals/arsenic/src/main/resources/db/migration/V20220117_01__data.sql
@@ -0,0 +1,2 @@
+INSERT INTO chemelement (id, name, symbol, atomnr) VALUES
+('157b2053-ba15-468d-86e3-bf8921be7306', 'Berkelium', 'Be', 4);
