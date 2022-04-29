c48a28e2b2cf6553e69c5e8222029acecee0d62a
Felix Roske <felix@zalando.de>
2022-04-29 19:19:25 +0200

    Add riak example

---
+++ chemicals/helloworld/hellokotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    val s = "World"
+    println("Hello ${s}!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
