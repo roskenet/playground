498bf759e4e3cf2c3d5a64ec590ffd0d5153b128
Felix Roske <felix@roskenet.de>
2022-04-30 15:24:02 +0200

    Switch hydrogen app to React18

--- chemicals/hydrogen/src/client/src/index.tsx
+++ chemicals/hydrogen/src/client/src/index.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import {createRoot} from 'react-dom/client';
 
-ReactDOM.render(
+const container = document.getElementById('root') as HTMLElement
+const root = createRoot(container);
+
+root.render(
     <Router>
-      <App />
-    </Router>,
-    document.getElementById('root')
+        <App/>
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
