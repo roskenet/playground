f80d78acff24ae090454763b3f0429abe48952af
Felix Roske <felix@roskenet.de>
2022-11-10 20:16:29 +0100

    Sync

--- chemicals/arsenic/src/main/java/de/roskenet/arsenic/ChemelementController.java
+++ chemicals/arsenic/src/main/java/de/roskenet/arsenic/ChemelementController.java
@@ -1,14 +1,16 @@
 package de.roskenet.arsenic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ChemelementController {
 
-    @Autowired
-    private ChemelementRepository repository;
+    private final ChemelementRepository repository;
+
+    public ChemelementController(ChemelementRepository repository) {
+        this.repository = repository;
+    }
 
     @GetMapping("/api/elements")
     public Iterable<Chemelement> getChemelements() {
--- java/java-features/src/main/java/de/zalando/showtell/MainClass.java
+++ java/java-features/src/main/java/de/zalando/showtell/MainClass.java
@@ -1,9 +1,6 @@
 package de.zalando.showtell;
 
-import java.util.Locale;
-
 public class MainClass {
-
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
