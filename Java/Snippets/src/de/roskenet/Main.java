5b3eae890652ef7fdeaab1ac858bfb3410219186
Felix Roske <felix@roskenet.de>
2023-02-26 22:34:51 +0100

    Add jpa examples

--- Java/Snippets/src/de/roskenet/Main.java
+++ Java/Snippets/src/de/roskenet/Main.java
@@ -9,7 +9,7 @@ public class Main {
         }
     }
 
-    public static void main(String[] args) {
+public static void main(String[] args) {
         String[] myArray = {"eins", "zwei", "drei"};
 
         doSomething(myArray);
---
+++ kotlin/challenges/src/main/kotlin/Main.kt
@@ -0,0 +1,13 @@
+import java.util.*
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
+
+fun myFun(input: String): String {
+   return input.uppercase(Locale.getDefault())
+}
