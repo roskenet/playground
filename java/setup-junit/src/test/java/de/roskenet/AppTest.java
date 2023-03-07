99a3cae0659238790101ccfbf2cd54afaa63523e
Felix Roske <felix@zalando.de>
2023-03-07 09:20:43 +0100

    Add jassert scaffolding

---
+++ java/setup-junit/src/test/java/de/roskenet/AppTest.java
@@ -0,0 +1,14 @@
+package de.roskenet;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest
+{
+    @Test
+    public void testApp()
+    {
+        assertThat(true).isTrue();
+    }
+}
