e4a85a362710afed580e1cde624f559ef5f0541b
Felix Roske <felix@roskenet.de>
2023-03-24 09:30:39 +0100

    Add Puzzle 8

---
+++ challenges/java-challenges/src/main/java/puzzles/Puzzle008.java
@@ -0,0 +1,10 @@
+package puzzles;
+
+public class Puzzle008 {
+    public static void main(String[] args) {
+        char x = 'X';
+        int i = 0;
+        System.out.print(true  ? x : 0);
+        System.out.print(false ? i : x);
+    }
+}
