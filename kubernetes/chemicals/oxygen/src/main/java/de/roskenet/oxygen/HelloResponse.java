3796c48e08e46d0412ea6f534448b46afbc01863
Felix Roske <felix@roskenet.de>
2023-04-02 11:50:00 +0200

    Archive old oxygen

--- kubernetes/chemicals/oxygen/src/main/java/de/roskenet/oxygen/HelloResponse.java
+++ kubernetes/chemicals/oxygen/src/main/java/de/roskenet/oxygen/HelloResponse.java
@@ -1,15 +1,4 @@
 package de.roskenet.oxygen;
 
-public class HelloResponse {
 
-    private String value;
-
-    public String getValue() {
-        return value;
-    }
-
-    public void setValue(String value) {
-        this.value = value;
-    }
-    
-}
+public record HelloResponse(String value) { }
