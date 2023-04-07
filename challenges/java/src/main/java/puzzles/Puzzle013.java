90e4f0444edccc53cac3895aaf583fe113a66d35
Felix Roske <felix@zalando.de>
2023-04-07 18:48:36 +0200

    Sync

--- challenges/java/src/main/java/puzzles/Puzzle013.java
+++ challenges/java/src/main/java/puzzles/Puzzle013.java
@@ -5,7 +5,6 @@ public class Puzzle013 {
         public static void main(String[ ] args) {
             final String pig = "length: 10";
             final String dog = "length: " + pig.length( );
-            System.out.println("Animals are equal: "
-                    + pig == dog);
+            System.out.println("Animals are equal: " + pig == dog);
         }
 }
