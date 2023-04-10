4eb2a136324052eededd56bbe4ec9a4297620273
Felix Roske <felix@roskenet.de>
2023-04-10 23:21:23 +0200

    Sync

---
+++ challenges/java/src/main/java/puzzles/Puzzle014.java
@@ -0,0 +1,8 @@
+package puzzles;
+
+public class Puzzle014 {
+    public static void main(String[ ] args) {
+        // \u0022 is the Unicode escape for double quote (")
+        System.out.println("a\u0022.length( ) + \u0022b".length( ));
+    }
+}
