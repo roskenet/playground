736668962fd273bddb0be0577f0c0449938c12f2
Felix Roske <felix@roskenet.de>
2023-04-18 22:03:52 +0200

    Play with LocalDate

--- kata/java-kata/src/main/java/de/roskenet/kata/FactorFinder.java
+++ kata/java-kata/src/main/java/de/roskenet/kata/FactorFinder.java
@@ -2,7 +2,6 @@ package de.roskenet.kata;
 
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.ArrayList;
