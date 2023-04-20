d120ef9ac535f75cc9d7fab8c8b60b8dbc667cbb
Felix Roske <felix@roskenet.de>
2023-04-20 10:10:10 +0200

    Add generic example

--- java/language-java/src/main/java/generics/Basics.java
+++ java/language-java/src/main/java/generics/Basics.java
@@ -3,14 +3,20 @@ package generics;
 import java.util.ArrayList;
 import java.util.List;
 
-class MyParamClass<T extends String> {
-    public T writeT() {
-    }
-}
+//class MyParamClass<T extends String> {
+//    public T writeT() {
+//    }
+//}
 
 public class Basics {
 
     public static void main(String[] args) {
-        List<String> stringList = new ArrayList<>();
+
+        Integer[] integerArray = new Integer[]{1, 2, 3};
+        Number[] numberArray = integerArray;
+
+        List<Integer> integerList = new ArrayList<>(List.of(1, 2, 3));
+        List<Number> numberList = integerList;
+
     }
 }
