9f4fd047ad611a7f97dd6f3d96253f2acbd0429c
Felix Roske <felix@roskenet.de>
2023-04-20 15:48:38 +0200

    Generics puzzle

--- java/language-java/src/main/java/generics/Basics.java
+++ java/language-java/src/main/java/generics/Basics.java
@@ -3,20 +3,21 @@ package generics;
 import java.util.ArrayList;
 import java.util.List;
 
-//class MyParamClass<T extends String> {
-//    public T writeT() {
-//    }
-//}
-
 public class Basics {
 
     public static void main(String[] args) {
 
+        // This works of course (because Integer is a subclass of Number:
+        Number number = Integer.valueOf(42);
+
+        // This works, too:
         Integer[] integerArray = new Integer[]{1, 2, 3};
         Number[] numberArray = integerArray;
 
+        // But this doesn't...
         List<Integer> integerList = new ArrayList<>(List.of(1, 2, 3));
         List<Number> numberList = integerList;
 
+        // Why? ;-)
     }
 }
