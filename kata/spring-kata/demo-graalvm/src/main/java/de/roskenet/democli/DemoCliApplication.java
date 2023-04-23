a3a39c89b524948f9d4a5230352a2030a1c20286
Felix Roske <felix@roskenet.de>
2023-04-23 23:48:50 +0200

    Add graalvm example

---
+++ kata/spring-kata/demo-graalvm/src/main/java/de/roskenet/democli/DemoCliApplication.java
@@ -0,0 +1,18 @@
+package de.roskenet.democli;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCliApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoCliApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
---
+++ kata/spring-kata/demo-graalvm/src/test/java/de/roskenet/democli/DemoCliApplicationTests.java
@@ -0,0 +1,13 @@
+package de.roskenet.democli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCliApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
