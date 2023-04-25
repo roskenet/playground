d236859c6188145d471faea5fffb4daa0a80754d
Felix Roske <felix@roskenet.de>
2023-04-25 23:52:47 +0200

    Add some javadoc

--- challenges/fourteenpuzzle/src/main/java/org/example/Puzzle.java
+++ challenges/fourteenpuzzle/src/main/java/org/example/Puzzle.java
@@ -2,6 +2,15 @@ package org.example;
 
 public class Puzzle {
 
+    /**
+     * Returns whether the puzzle is solved.
+     *
+     * @param configuration
+     * @return true if the integer matrix is ascending from
+     *         top left to bottom right with the 0 as the last field.
+     *         else false
+     * @throws IllegalArgumentException if the configuration is {@code null} or not valid at all.
+     */
     public static boolean isSolved(int[][] configuration) {
         if(configuration == null) {
             throw new IllegalArgumentException("Configuration must not be null.");
