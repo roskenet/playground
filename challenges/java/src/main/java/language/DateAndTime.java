09317bcfd409afdd2293b2063e05370accf5501a
Felix Roske <felix@roskenet.de>
2023-04-26 19:02:39 +0200

    Resolve merge conflicts

--- challenges/java/src/main/java/language/DateAndTime.java
+++ challenges/java/src/main/java/language/DateAndTime.java
@@ -7,8 +7,6 @@ import java.util.Date;
 public class DateAndTime {
 
     public static void main(String[] args) {
-        Date myDate = null;
-
         // Resolution to miilisecs.
         var parsedDateTime = ZonedDateTime.parse("2023-04-02T00:35:04.123456+02:00");
 
