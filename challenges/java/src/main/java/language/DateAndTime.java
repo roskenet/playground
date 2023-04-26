9cdeaa30fc9f47887d5ed2e7d32991f56e7a73c0
Felix Roske <felix@roskenet.de>
2023-04-26 19:17:59 +0200

    Play with ZonedDateTime

--- challenges/java/src/main/java/language/DateAndTime.java
+++ challenges/java/src/main/java/language/DateAndTime.java
@@ -1,16 +1,17 @@
 package language;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.ZonedDateTime;
-import java.util.Date;
 
 public class DateAndTime {
 
     public static void main(String[] args) {
-        // Resolution to miilisecs.
-        var parsedDateTime = ZonedDateTime.parse("2023-04-02T00:35:04.123456+02:00");
+        var parsedDateTime = ZonedDateTime.parse("2023-04-01T23:35:04.123456+00:00");
+        var londonDateTime = parsedDateTime.withZoneSameInstant(ZoneId.of("Europe/London"));
+        var berlinDateTime = parsedDateTime.withZoneSameInstant(ZoneId.of("Europe/Berlin"));
 
-        var myTime = LocalDateTime.from(parsedDateTime);
-        System.out.println(myTime);
+        System.out.println(String.format("In Berlin ist es dann: %s.", berlinDateTime));
+        System.out.println(String.format("In London ist es dann: %s.", londonDateTime));
     }
 }
