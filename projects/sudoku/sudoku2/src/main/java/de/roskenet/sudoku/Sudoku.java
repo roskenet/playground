241f917974b6939724c8dd368f0c21f9a48f3949
Felix Roske <felix@roskenet.de>
2023-05-01 23:52:52 +0200

    Add third sudoku alternative version

--- projects/sudoku/sudoku2/src/main/java/de/roskenet/sudoku/Sudoku.java
+++ projects/sudoku/sudoku2/src/main/java/de/roskenet/sudoku/Sudoku.java
@@ -1,24 +1,13 @@
 package de.roskenet.sudoku;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 public class Sudoku {
 
-    private Set<Integer>[][] marks = new Set[9][9];
+    private int[][] field;
 
     public Sudoku() {
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
-                marks[row][col] =
-                        new HashSet<>(Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9));
-            }
-        }
+        field = new int[9][9];
     }
-
-   public Set<Integer> getMarks(int row, int col) {
-        return marks[row][col];
-   }
-
+    public Sudoku(int[][] sudoku) {
+       field = sudoku;
+    }
 }
