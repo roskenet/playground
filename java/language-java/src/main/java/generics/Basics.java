4ece863c2a4bab59ff3b36585ee7bf485afa6ac9
Felix Roske <felix@roskenet.de>
2023-06-15 23:38:19 +0200

    Sync

--- java/language-java/src/main/java/generics/Basics.java
+++ java/language-java/src/main/java/generics/Basics.java
@@ -5,19 +5,27 @@ import java.util.List;
 
 public class Basics {
 
+    /*
+        public static void main(String[] args) {
+
+            // This works of course (because Integer is a subclass of Number:
+            Number number = Integer.valueOf(42);
+
+            // This works, too:
+            Integer[] integerArray = new Integer[]{1, 2, 3};
+            Number[] numberArray = integerArray;
+
+            // But this doesn't...
+            List<Integer> integerList = new ArrayList<>(List.of(1, 2, 3));
+            List<Number> numberList = integerList;
+
+            // Why? ;-)
+        }
+    */
     public static void main(String[] args) {
+        List<String> myList = new ArrayList<>();
+        myList.add("Hello");
 
-        // This works of course (because Integer is a subclass of Number:
-        Number number = Integer.valueOf(42);
-
-        // This works, too:
-        Integer[] integerArray = new Integer[]{1, 2, 3};
-        Number[] numberArray = integerArray;
-
-        // But this doesn't...
-        List<Integer> integerList = new ArrayList<>(List.of(1, 2, 3));
-        List<Number> numberList = integerList;
-
-        // Why? ;-)
+        myList.forEach(System.out::println);
     }
 }
