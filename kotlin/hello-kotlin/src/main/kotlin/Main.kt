929a26a573aa93bef0b63eee989a9f8468a798a8
Felix Roske <felix@zalando.de>
2023-07-06 10:43:48 +0200

    Add simplicity wisdom

--- kotlin/hello-kotlin/src/main/kotlin/Main.kt
+++ kotlin/hello-kotlin/src/main/kotlin/Main.kt
@@ -1,9 +1,11 @@
 fun main(args: Array<String>) {
     println("Hello World!")
 
-    var myInt: Int = 42
+    val cities = listOf("New York", "Rio", "Tokyo")
+
+    val capitals = mutableListOf("Berlin", "Bern", "Beijing")
 
     // Try adding program arguments via Run/Debug configuration.
     // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    println("Capitals with B: ${capitals.joinToString()}")
 }
---
+++ wisdom/simplicity.md
@@ -0,0 +1,4 @@
+# Simplicity
+
+Simplicity—the art of maximizing the amount of work not done—is essential. 
+
