79a135bb879a8c9168e8ff41ee6f2523b618ce43
Felix Roske <felix@roskenet.de>
2024-01-30 23:57:29 +0100

    Sync

--- go/learning-go/Main.go
+++ go/learning-go/Main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"fmt"
+
 	"myapp.roskenet.de/subdir"
 )
 
 func main() {
 
+	myslice := []int16{1, 2, 3, 4}
+
+	myslice = append(myslice, 42)
+
+	for i, num := range myslice {
+		fmt.Printf("%d ist %d\n", i, num)
+	}
+
 	elvis := new(subdir.Singer)
-	//elvis.Name = "Elvis Presley"
+	elvis.Name = "Elvis Presley"
 	elvis.Song = "Jailhouse Rock"
 
-	fmt.Printf("%s sings %s", elvis.Name, elvis.Song)
+	madonna := subdir.Singer{
+		Name: "Madonna",
+		Song: "Like a Virgin",
+	}
 
+	fmt.Printf("%s sings %s \n", elvis.Name, elvis.Song)
+	fmt.Printf("Madonna: %s", madonna)
 }
