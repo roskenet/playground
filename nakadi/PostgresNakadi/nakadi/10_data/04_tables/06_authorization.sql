cf7587a9e9d94b269c4472450f33c42af527b635
Felix Roske <felix@zalando.de>
2024-02-06 23:34:29 +0100

    Add minikube nakadi

---
+++ nakadi/PostgresNakadi/nakadi/10_data/04_tables/06_authorization.sql
@@ -0,0 +1,11 @@
+CREATE TYPE zn_data.az_operation AS ENUM('ADMIN', 'READ', 'WRITE');
+
+CREATE TABLE zn_data.authorization (
+  az_resource        VARCHAR(255)             NOT NULL,
+  az_operation       zn_data.az_operation             NOT NULL,
+  az_data_type       VARCHAR(255)             NOT NULL,
+  az_value           VARCHAR(255)             NOT NULL,
+  PRIMARY KEY (az_resource, az_operation, az_data_type, az_value)
+);
+
+CREATE INDEX ON zn_data.authorization (az_resource);
