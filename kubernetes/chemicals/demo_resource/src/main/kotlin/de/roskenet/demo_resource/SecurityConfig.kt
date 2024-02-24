fb912d7594d047393021e8e5d6166cd003a5fe59
Felix Roske <felix@roskenet.de>
2024-02-24 17:07:48 +0100

    Add most minimal auth2 demo

--- kubernetes/chemicals/demo_resource/src/main/kotlin/de/roskenet/demo_resource/SecurityConfig.kt
+++ kubernetes/chemicals/demo_resource/src/main/kotlin/de/roskenet/demo_resource/SecurityConfig.kt
@@ -2,9 +2,7 @@ package de.roskenet.demo_resource
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 
 @Configuration
-@EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 class SecurityConfig
