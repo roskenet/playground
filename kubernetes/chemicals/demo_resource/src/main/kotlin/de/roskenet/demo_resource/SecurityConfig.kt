bf47aae5bf6002d02fef9f59cd884d17b1fed88b
Felix Roske <felix@roskenet.de>
2024-02-24 17:04:43 +0100

    Remove unused imports

--- kubernetes/chemicals/demo_resource/src/main/kotlin/de/roskenet/demo_resource/SecurityConfig.kt
+++ kubernetes/chemicals/demo_resource/src/main/kotlin/de/roskenet/demo_resource/SecurityConfig.kt
@@ -1,15 +1,8 @@
 package de.roskenet.demo_resource
 
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity
-import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.web.SecurityFilterChain
-
-import org.springframework.security.config.annotation.web.invoke
-import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector
-import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector
 
 @Configuration
 @EnableWebSecurity
