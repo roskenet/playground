ae2823988b6b79d2e8bcbbfd2ac67765cf56c391
Felix Roske <felix@roskenet.de>
2024-03-23 16:01:23 +0100

    Sync with new Cpp and CMake stuff

--- cmake/first_build/src/main.cpp
+++ cmake/first_build/src/main.cpp
@@ -1,6 +1,48 @@
 #include<iostream>
+#include<string>
+#include <iomanip>
+
+enum class Manufacturer {
+   Sony,
+   Hitachi,
+   Samsung,
+   Leica,
+
+};
+
+// prototype
+void doSomething(int&);
 
 int main() {
     std::cout << "Hello World!" << std::endl;
+
+    u_int8_t myInt{42};
+
+    float myFloat{3.1415926};
+
+    std::setprecision(2);
+
+    std::cout << "Value is " << std::setprecision(2) << myFloat << std::endl;
+
+    std::cout << "Sticky settings: " << myFloat << std::endl;
+
+    const Manufacturer myCamera{Manufacturer::Samsung};
+
+    if(myCamera == Manufacturer::Leica) {
+        std::cout << "I don't own a Leica!";
+    } else if (myCamera == Manufacturer::Samsung) {
+        std::cout << "I own a Samsung!" << std::endl;
+    }
+
+    int theAnswer{42};
+    doSomething(theAnswer);
+
+    std::cout << "But now theAnswer is: " << theAnswer;
+
     return 0;
+}
+
+void doSomething(int& myReference) {
+    std::cout << "The reference is: " << myReference << std::endl;
+    myReference = 43;
 }
