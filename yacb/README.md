3d47ed4e5acd9d6ab9813816de1abdabff391814
Felix Roske <felix@zalando.de>
2024-03-27 15:37:33 +0100

    Add YACB

---
+++ yacb/README.md
@@ -0,0 +1,5 @@
+# Yet Another Computer Book
+
+# C 100
+
+# C 200 - Spring CommandLineRunner
---
+++ yacb/c_200/kotlin-demo/src/main/kotlin/de/felixroske/kotlindemo/KotlinDemoApplication.kt
@@ -0,0 +1,17 @@
+package de.felixroske.kotlindemo
+
+import org.springframework.boot.CommandLineRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinDemoApplication: CommandLineRunner {
+    override fun run(vararg args: String?) {
+        println("Hello World!")
+    }
+
+}
+
+fun main(args: Array<String>) {
+    runApplication<KotlinDemoApplication>(*args)
+}
