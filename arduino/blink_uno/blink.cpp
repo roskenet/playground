403ad919c399a87d03352db471842c7ced41b08e
Felix Roske <felix@roskenet.de>
2024-03-29 20:55:34 +0100

    Add an arduino UNO project

---
+++ arduino/blink_uno/blink.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+void setup() {
+    Serial.begin(115200);
+    delay(2000);
+    Serial.println("Hello, world!");
+    pinMode(13, OUTPUT);
+}
+
+void loop() {
+    digitalWrite(13, HIGH);
+    delay(500);
+    digitalWrite(13, LOW);
+    delay(500);
+}
