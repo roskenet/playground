6039e1517981c1ae59fc5b390811fd10780bc139
Felix Roske <felix@roskenet.de>
2024-04-18 23:15:09 +0200

    Add complete standalone nakadi

--- kubernetes/nakadi/database-setup/execute_db_scripts_in_container.sh
+++ kubernetes/nakadi/database-setup/execute_db_scripts_in_container.sh
@@ -1,2 +1,5 @@
+# CREATE USER nakadi WITH PASSWORD 'nakadi' CREATEROLE;
+# CREATE DATABASE nakadi owner nakadi;
+
 find . -type f -name "*.sql" | sort |
 while read -r filename; do psql -h localhost -U nakadi -d nakadi -a -f "$filename"; done
