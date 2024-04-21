943e3574b62105d3a6b035f5de2addc4eaa0d81f
Felix Roske <felix@roskenet.de>
2024-04-21 10:08:17 +0200

    Add cuppa project again

---
+++ arduino/cuppaV3/cmake/toolchain/nano.toolchain.cmake
@@ -0,0 +1,10 @@
+set(ARDUINO_BOARD "AVR_UNO")
+set(ARDUINO_MCU "atmega328p")
+set(ARDUINO_F_CPU "16000000L")
+set(ARDUINO_VARIANT "standard")
+set(ARDUINO_AVRDUDE_PROTOCOL "arduino")
+set(ARDUINO_AVRDUDE_SPEED "57600")
+set(ARDUINO_USB Off)
+set(ARDUINO_PORT "/dev/ttyUSB0")
+
+include(${CMAKE_CURRENT_LIST_DIR}/avr.toolchain.cmake)
---
+++ kata/kata_002/kotlin-multiline-demo/src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+package org.example
+
+const val description: String = """
+    This is a multiline example.
+    This code gets printed as you see it here.
+"""
+
+fun main() {
+    println(description)
+}
