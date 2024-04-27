f4fb919c019bb3cbcc9b68cb1d4b2c6af4a27088
Felix Roske <felix@roskenet.de>
2024-04-27 13:17:05 +0200

    Add android examples

---
+++ Cpp/hello-cpp/hello-cpp.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<string>
+#include<limits>
+#include<vector>
+using namespace std;
+
+
+int main(int argc, char **argv) {
+
+    vector<int> v(10);
+
+	return 0;
+}
---
+++ Cpp/playground/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    uid_t myint;
+    std::cout << "Die Adresse ist: " << &myint;
+    return 0;
+}
