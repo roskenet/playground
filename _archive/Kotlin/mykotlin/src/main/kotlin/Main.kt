4591d85c3d38f1f010e8cfd9c6809fd6e2ae0fff
Felix Roske <felix@roskenet.de>
2024-05-29 23:32:16 +0200

    Add Cpp example

--- _archive/Kotlin/mykotlin/src/main/kotlin/Main.kt
+++ _archive/Kotlin/mykotlin/src/main/kotlin/Main.kt
@@ -2,14 +2,20 @@
 
 private const val votingAgeGermany = 18 // only visible in Main.kt
 
-fun mayVote(age: Int = 18, name: String) : String {
-    if (age >= votingAgeGermany) {
-        return "Yes, ${name} can vote."
-    }
-    else {
-        return "No, ${name} can't vote."
-    }
-}
+//fun mayVote(age: Int = 18, name: String) : String {
+//    if (age >= votingAgeGermany) {
+//        return "Yes, ${name} can vote."
+//    }
+//    else {
+//        return "No, ${name} can't vote."
+//    }
+//}
+
+fun mayVote(age: Int = votingAgeGermany, name: String) =
+    if (age >= votingAgeGermany)
+        "Yes $name may vote!"
+    else
+        "No, $name can't vote"
 
 fun main(args: Array<String>) =
     println("Hello World! ${mayVote(name = "John")}")
