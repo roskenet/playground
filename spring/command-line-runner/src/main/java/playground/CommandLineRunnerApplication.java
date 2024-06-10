1469d07cc185989cd4f8431dfba74e4999cf79eb
Felix Roske <felix@zalando.de>
2024-06-10 21:21:53 +0200

    Add spring boot command line runner

--- spring/command-line-runner/src/main/java/playground/CommandLineRunnerApplication.java
+++ spring/command-line-runner/src/main/java/playground/CommandLineRunnerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.time.Instant;
-
 @SpringBootApplication
 public class CommandLineRunnerApplication implements CommandLineRunner {
 
