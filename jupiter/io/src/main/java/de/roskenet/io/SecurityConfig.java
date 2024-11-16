e161a87f139b05230c70b33d1bc7cd8a9f319b8d
Felix Roske <felix@zalando.de>
2024-11-16 23:37:07 +0100

    Add io client for ganymede

---
+++ jupiter/io/src/main/java/de/roskenet/io/SecurityConfig.java
@@ -0,0 +1,22 @@
+package de.roskenet.io;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers("/", "/hello").permitAll()
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+
+}
