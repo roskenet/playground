a1dbf1b29fb6df99070cdd91d93279d4de1df2b2
Felix Roske <felix@zalando.de>
2024-11-16 22:43:58 +0100

    Add ganymede minikube deployment

--- jupiter/ganymede/src/main/java/de/roskenet/ganymede/SecurityConfig.java
+++ jupiter/ganymede/src/main/java/de/roskenet/ganymede/SecurityConfig.java
@@ -1,12 +1,8 @@
 package de.roskenet.ganymede;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity(prePostEnabled = true)
+@EnableMethodSecurity
 public class SecurityConfig {}
