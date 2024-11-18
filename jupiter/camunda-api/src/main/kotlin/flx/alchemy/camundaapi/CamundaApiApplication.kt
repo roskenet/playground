3914b81ef4570300cefae445d741f76c820bf975
Felix Roske <felix@zalando.de>
2024-11-18 16:58:41 +0100

    Sync

---
+++ jupiter/camunda-api/src/main/kotlin/flx/alchemy/camundaapi/CamundaApiApplication.kt
@@ -0,0 +1,11 @@
+package flx.alchemy.camundaapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CamundaApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<CamundaApiApplication>(*args)
+}
---
+++ jupiter/camunda-api/src/test/kotlin/flx/alchemy/camundaapi/CamundaApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package flx.alchemy.camundaapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CamundaApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
