ab2b3009b20a7d788095f5587110d02a41d77ed6
Felix Roske <felix@zalando.de>
2024-11-26 20:45:42 +0100

    Add mumble

--- kata/kata-in-kotlin/src/main/kotlin/Mumbling.kt
+++ kata/kata-in-kotlin/src/main/kotlin/Mumbling.kt
@@ -5,13 +5,21 @@ import kotlin.test.assertEquals
 
 // https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/train/kotlin
 
-fun accum(s:String):String {
-    return ""
+fun accum(s: String): String {
+    val array = MutableList(0) { "" }
+
+    s.forEachIndexed {
+        i, c -> array.add(c.uppercase() + c.lowercase().repeat(i))
+    }
+
+    return array.joinToString("-")
 }
+//fun accum(s:String):String =
+// s.mapIndexed { index, char -> char.toUpperCase() + char.toString().toLowerCase().repeat(index) }.joinToString("-")
 
 class AccumulTest {
 
-    private fun testing(actual:String, expected:String) {
+    private fun testing(actual: String, expected: String) {
         assertEquals(expected, actual)
     }
 
@@ -20,6 +28,5 @@ class AccumulTest {
         println("Fixed Tests accum")
         testing(accum("ZpglnRxqenU"), "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu")
         testing(accum("NyffsGeyylB"), "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb")
-
     }
 }
