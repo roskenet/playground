c7f6a9b095eb7689e9454444aae6e16141986465
Felix Roske <felix@zalando.de>
2024-11-28 16:14:01 +0100

    Secure actinium service

--- alchemy/actinium/src/main/kotlin/de/felixroske/actinium/ArtistApi.kt
+++ alchemy/actinium/src/main/kotlin/de/felixroske/actinium/ArtistApi.kt
@@ -1,5 +1,6 @@
 package de.felixroske.actinium
 
+import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -7,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController
 class ArtistApi {
 
     @GetMapping("/artists")
+    @PreAuthorize("hasAuthority('SCOPE_profile')")
     fun getArtists(): List<Artist> = listOf(
         Artist("Amy Winehouse", 1983),
         Artist("Elvis Presley", 1935)
