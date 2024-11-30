7d3b675e8d1f5ed89a489033c0053f0589f8c3da
Felix Roske <felix@zalando.de>
2024-11-30 23:38:11 +0100

    Add next.js Example

---
+++ react/flx-next-app/app/layout.jsx
@@ -0,0 +1,11 @@
+const MainLayout = ({children}) => {
+    return (
+        <html lang='en'>
+            <body>
+                <div>{children}</div>
+            </body>
+        </html>
+    )
+}
+
+export default MainLayout
