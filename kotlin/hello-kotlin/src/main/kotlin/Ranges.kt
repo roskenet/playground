fa62b4606f8aaec83839fa9b71609c13050b4a28
Felix Roske <felix@zalando.de>
2024-12-16 15:27:54 +0100

    Add coroutine example

--- kotlin/hello-kotlin/src/main/kotlin/Ranges.kt
+++ kotlin/hello-kotlin/src/main/kotlin/Ranges.kt
@@ -1,12 +1,17 @@
 package de.felixroske
 
 fun main() {
-    // This is a range:
-    val myRange = 0.rangeTo(10)
-
-    val isInRange = checkRange(1, myRange)
-    println(isInRange)
+    executeSomething(::rangePlayground)
 }
 
-fun checkRange(x: Int, r: ClosedRange<Int>) =
-    if (x in r) true else false
+fun rangePlayground() {
+    for(i in 1..100 step 2) {
+        println(i)
+    }
+}
+
+typealias MyFunc = () -> Unit
+
+fun executeSomething(myFunc: MyFunc) {
+    myFunc()
+}
