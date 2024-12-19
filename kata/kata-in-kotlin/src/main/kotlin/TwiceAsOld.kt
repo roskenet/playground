261bbb6483b43da98395a4287335f218f25267dd
Felix Roske <felix@zalando.de>
2024-12-19 22:25:15 +0100

    Add more kata

---
+++ kata/kata-in-kotlin/src/main/kotlin/TwiceAsOld.kt
@@ -0,0 +1,20 @@
+package kata.twiceasold
+
+import kotlin.math.abs
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/kotlin
+
+fun twiceAsOld(dadYearsOld: Int, sonYearsOld: Int): Int = abs(2 * sonYearsOld - dadYearsOld)
+
+class TestExample {
+    @Test
+    fun testFixed() {
+        assertEquals(22, twiceAsOld(36,7))
+        assertEquals(5, twiceAsOld(55,30))
+        assertEquals(0, twiceAsOld(42,21))
+        assertEquals(20, twiceAsOld(22,1))
+        assertEquals(29, twiceAsOld(29,0))
+    }
+}
