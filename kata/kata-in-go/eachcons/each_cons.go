6d41d849eba9d813f085428e00b20844ef9a4c00
Felix Roske <felix@zalando.de>
2025-01-05 17:48:56 +0100

    Add EachCons kata

--- kata/kata-in-go/eachcons/each_cons.go
+++ kata/kata-in-go/eachcons/each_cons.go
@@ -9,24 +9,18 @@ func RunEachConsTest() {
 	fmt.Println("EachCons")
 	arr := []int{3, 5, 8, 13, 16}
 
-	res := EachCons(arr, 3)
+	res := EachCons(arr, 4)
 
 	fmt.Println(res)
 }
 
 func EachCons(arr []int, n int) [][]int {
 
-	outerArr := [][]int{}
+	result := [][]int{}
 
-	for idx := 0; idx < len(arr); idx++ {
-		if len(arr)-idx >= n {
-			innerArr := []int{}
-			for idx2 := 0; idx2 < n; idx2++ {
-				innerArr = append(innerArr, arr[idx+idx2])
-			}
-			outerArr = append(outerArr, innerArr)
-		}
+	for i := 0; i <= len(arr)-n; i++ {
+		result = append(result, arr[i:i+n])
 	}
 
-	return outerArr
+	return result
 }
