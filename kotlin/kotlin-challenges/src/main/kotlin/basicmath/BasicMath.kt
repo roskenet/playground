c79d46e45275d0f542e9669cd99cf4df32e135b2
Felix Roske <felix@zalando.de>
2025-02-14 00:13:24 +0100

    Add kotlin challenges

---
+++ kotlin/kotlin-challenges/src/main/kotlin/basicmath/BasicMath.kt
@@ -0,0 +1,10 @@
+package de.roskenet.basicmath.divandmod
+
+fun main() {
+
+    val resultDiv = (6 * 7 + 3) / 7
+    val resultMod = (6 * 7 + 3) % 7
+
+    println("ResultDiv: $resultDiv")
+    println("ResultMod: $resultMod")
+}
